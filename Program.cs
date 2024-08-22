dbc22370a67223082382ef76339b61e20deaecb6
Rafael F Pessoa <112719981+RafaFonPessoa@users.noreply.github.com>
2024-08-22 15:09:55 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("85999999999","Tijolão","12345678", 64);
+
+Iphone iphone = new Iphone("859999999","7","123456788",128);
+
+nokia.InstalarAplicativo("Cobrinha");
+iphone.InstalarAplicativo("Whatsapp");
